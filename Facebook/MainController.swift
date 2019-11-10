ab8bdf635a8d7020582803d013acbd2273bd83b5
Тимур Чеберда <timurceberda@MacBook-Air-Timur.local>
2019-11-10 23:00:40 +0300

    add a table, cell, some spacing for the cell

---
+++ Facebook/MainController.swift
@@ -0,0 +1,30 @@
+//
+//  MainController.swift
+//  Facebook
+//
+//  Created by Тимур Чеберда on 10.11.2019.
+//  Copyright © 2019 Тимур Чеберда. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Facebook/PhotosGridController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotosGridController.swift
+//  Facebook
+//
+//  Created by Тимур Чеберда on 10.11.2019.
+//  Copyright © 2019 Тимур Чеберда. All rights reserved.
+//
+
+import Foundation

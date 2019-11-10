fdc98428ba5f28b0eb01d2c4b7b839411fdc35e6
Timur <likedune@gmail.com>
2019-11-10 23:06:03 +0300

    add missed files

--- Facebook/MainController.swift
+++ Facebook/MainController.swift
@@ -7,24 +7,70 @@
 //
 
 import UIKit
+import LBTATools
 
-class MainController: UIViewController {
+class PostCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(backgroundColor: .red)
+    let nameLabel = UILabel(text: "Name Label")
+    let dateLabel = UILabel(text: "Friday at 11:11 AM")
+    let postTextLabel = UILabel(text: "Here is my post text")
+//    let imageViewGrid = UIView(backgroundColor: .yellow)
+    
+    let photosGridController = PhotosGridController()
+    
+    override func setupViews() {
+        backgroundColor = .white
+        
+        stack(
+            hstack(
+                imageView
+                    .withHeight(40)
+                    .withWidth(40),
+                stack(nameLabel, dateLabel),
+                spacing: 8
+                )
+                .padLeft(12)
+                .padRight(12)
+                .padTop(12),
+            postTextLabel,
+            photosGridController.view,
+            spacing: 8
+        )
+    }
+}
 
+class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
+        
+        self.items = ["hello", "WORLD", "1", "2"]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return .init(width: view.frame.width, height: 250)
     }
-    */
+}
 
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContrinerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContrinerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContrinerView>) -> UIViewController {
+            return MainController()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainPreview.ContrinerView.UIViewControllerType, context: UIViewControllerRepresentableContext<MainPreview.ContrinerView>) {
+            
+        }
+    }
 }
--- Facebook/PhotosGridController.swift
+++ Facebook/PhotosGridController.swift
@@ -6,4 +6,73 @@
 //  Copyright © 2019 Тимур Чеберда. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
+import LBTATools
+
+class PhotoGridCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(
+        image: <#T##UIImage?#>,
+        contentMode: .scaleAspectFill
+    )
+
+    override func setupViews() {
+        backgroundColor = .yellow
+    }
+}
+
+class PhotosGridController: LBTAListController<PhotoGridCell, String>, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .lightGray
+        
+        self.items = ["1", "2", "3"]
+    }
+    
+    let cellSpacing: CGFloat = 4
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (view.frame.width - 4 * cellSpacing) / 3
+        
+        return .init(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return .init(
+            top: 0,
+            left: cellSpacing,
+            bottom: 0,
+            right: cellSpacing
+        )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
+}
+
+struct PhotosGridPreview: PreviewProvider {
+    
+    static var previews: some View {
+        
+        ContainerView()
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<PhotosGridPreview.ContainerView>) -> UIViewController {
+            
+            return PhotosGridController()
+        }
+        
+        func updateUIViewController(_ uiViewController: PhotosGridPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<PhotosGridPreview.ContainerView>) {
+            
+        }
+    }
+}

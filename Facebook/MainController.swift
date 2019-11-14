aac6c842952c7423a99040b7ec5f284d9c464f8d
Timur <likedune@gmail.com>
2019-11-14 23:04:20 +0300

    Headers and Gradient Layers

--- Facebook/MainController.swift
+++ Facebook/MainController.swift
@@ -11,36 +11,114 @@ import LBTATools
 
 class PostCell: LBTAListCell<String> {
     
-    let imageView = UIImageView(backgroundColor: .red)
+    let imageView = UIImageView(backgroundColor: .blue)
     let nameLabel = UILabel(text: "Name Label")
-    let dateLabel = UILabel(text: "Friday at 11:11 AM")
+    let dateLabel = UILabel(text: "Friday at 11:11AM")
     let postTextLabel = UILabel(text: "Here is my post text")
-//    let imageViewGrid = UIView(backgroundColor: .yellow)
+    
+    //    let imageViewGrid = UIView(backgroundColor: .yellow)
     
     let photosGridController = PhotosGridController()
     
     override func setupViews() {
         backgroundColor = .white
         
-        stack(
-            hstack(
-                imageView
-                    .withHeight(40)
-                    .withWidth(40),
-                stack(nameLabel, dateLabel),
-                spacing: 8
-                )
-                .padLeft(12)
-                .padRight(12)
-                .padTop(12),
-            postTextLabel,
-            photosGridController.view,
-            spacing: 8
-        )
+        stack(hstack(imageView.withHeight(40).withWidth(40),
+                     stack(nameLabel, dateLabel),
+                     spacing: 8).padLeft(12).padRight(12).padTop(12),
+              postTextLabel,
+              photosGridController.view,
+              spacing: 8)
+        
     }
 }
 
-class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+class StoryHeader: UICollectionReusableView {
+    
+    let storiesController = StoriesController(scrollDirection: .horizontal)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        stack(storiesController.view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class StoryPhotoCell: LBTAListCell<String> {
+    
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+        }
+    }
+    
+    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(
+        text: "Maddyson is near",
+        font: .boldSystemFont(ofSize: 14),
+        textColor: .white
+    )
+    
+    override func setupViews() {
+        
+        imageView.layer.cornerRadius = 10
+        
+        stack(imageView)
+        
+        setupGradientLayer()
+        
+        stack(
+            UIView(),
+            nameLabel).withMargins(.allSides(8))
+    }
+    
+    let gradientLayer = CAGradientLayer()
+    
+    fileprivate func setupGradientLayer() {
+        
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.7, 1.1]
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+}
+
+class StoriesController: LBTAListController<StoryPhotoCell, String>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 100, height: view.frame.height - 24)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 12, bottom: 0, right: 12)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.items = ["mad3", "mad2", "mad1"]
+    }
+    
+}
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,25 +129,24 @@ class MainController: LBTAListController<PostCell, String>, UICollectionViewDele
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        return .init(width: view.frame.width, height: 250)
+        return .init(width: view.frame.width, height: 400)
     }
 }
 
 import SwiftUI
-
 struct MainPreview: PreviewProvider {
     static var previews: some View {
-        ContrinerView().edgesIgnoringSafeArea(.all)
+        //        Text("main preview 123123")
+        ContainerView().edgesIgnoringSafeArea(.all)
     }
     
-    struct ContrinerView: UIViewControllerRepresentable {
+    struct ContainerView: UIViewControllerRepresentable {
         
-        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContrinerView>) -> UIViewController {
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> UIViewController {
             return MainController()
         }
         
-        func updateUIViewController(_ uiViewController: MainPreview.ContrinerView.UIViewControllerType, context: UIViewControllerRepresentableContext<MainPreview.ContrinerView>) {
+        func updateUIViewController(_ uiViewController: MainPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) {
             
         }
     }
--- Facebook/PhotosGridController.swift
+++ Facebook/PhotosGridController.swift
@@ -12,13 +12,16 @@ import LBTATools
 
 class PhotoGridCell: LBTAListCell<String> {
     
-    let imageView = UIImageView(
-        image: <#T##UIImage?#>,
-        contentMode: .scaleAspectFill
-    )
-
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+        }
+    }
+    
+    let imageView = UIImageView(image: UIImage(named: "avatar1"), contentMode: .scaleAspectFill)
+    
     override func setupViews() {
-        backgroundColor = .yellow
+        stack(imageView)
     }
 }
 
@@ -26,48 +29,53 @@ class PhotosGridController: LBTAListController<PhotoGridCell, String>, UICollect
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         collectionView.backgroundColor = .lightGray
         
-        self.items = ["1", "2", "3"]
+        self.items = ["mad1", "mad2", "mad3", "mad2", "mad1"]
     }
     
     let cellSpacing: CGFloat = 4
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let width = (view.frame.width - 4 * cellSpacing) / 3
+        if self.items.count == 4 {
+            // do 4 grid cell layout
+        }
+        
+        if indexPath.item == 0 || indexPath.item == 1 {
+            let width = (view.frame.width - 3 * cellSpacing) / 2
+            
+            return .init(width: width, height: width)
+        }
+        
+        let width = (view.frame.width - 4.1 * cellSpacing) / 3
         
         return .init(width: width, height: width)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        
-        return .init(
-            top: 0,
-            left: cellSpacing,
-            bottom: 0,
-            right: cellSpacing
-        )
+        return .init(top: 0, left: cellSpacing, bottom: 0, right: cellSpacing)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        
-        return 0
+        return cellSpacing
     }
+    
 }
 
 struct PhotosGridPreview: PreviewProvider {
     
     static var previews: some View {
-        
         ContainerView()
     }
     
     struct ContainerView: UIViewControllerRepresentable {
         
         func makeUIViewController(context: UIViewControllerRepresentableContext<PhotosGridPreview.ContainerView>) -> UIViewController {
-            
             return PhotosGridController()
         }
         
